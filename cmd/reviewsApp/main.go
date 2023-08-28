3c57dbaba0c3919826d00b02b7a53783c4191441
Ahsan Amin <ahsan.amin@careem.com>
2023-08-28 15:00:09 +0500

    setup project

--- cmd/reviewsApp/main.go
+++ cmd/reviewsApp/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/julienschmidt/httprouter"
 
-	"github.com/ahsanaminofficial/golang-project-careem/tree/main/pkg/controllers"
+	"github.com/ahsanaminofficial/golang-project-careem/pkg/controllers"
 )
 
 func main() {
@@ -14,4 +15,13 @@ func main() {
 
 	router := httprouter.New()
 	router.GET("/", controllers.HomepageHandler)
+	router.GET("/reviews", controllers.AllReviews)
+	router.GET("/reviews/:id", controllers.ReviewById)
+	router.POST("/review", controllers.CreateReview)
+
+	err := http.ListenAndServe(":3000", router)
+
+	if err != nil {
+		fmt.Println("Count not start server")
+	}
 }
--- pkg/controllers/reviewsController.go
+++ pkg/controllers/reviewsController.go
@@ -1,5 +1,29 @@
 package controllers
 
-func HomepageHandler() {
+import (
+	"fmt"
+	"io"
+	"net/http"
 
+	"github.com/julienschmidt/httprouter"
+)
+
+func HomepageHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	io.WriteString(w, "Home page")
+	fmt.Println("homepage handler")
+}
+
+func AllReviews(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	io.WriteString(w, "AllReviews")
+	fmt.Println("AllReviews")
+}
+
+func ReviewById(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	io.WriteString(w, "ReviewById")
+	fmt.Println("ReviewById")
+}
+
+func CreateReview(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	io.WriteString(w, "CreateReview")
+	fmt.Println("CreateReview")
 }
